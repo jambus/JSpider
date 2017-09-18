fcc5444f3ab184fdfe744694756137f574063029
Liang Shen <jambus2010@gmail.com>
2017-09-18 17:43:48 +0800

    spider for lianjia init version

---
+++ HouseSpider/script/lianjiaSpider.py
@@ -0,0 +1,36 @@
+import urllib.parse as urlparse
+
+from pyspider.libs.base_handler import *
+
+
+class Handler(BaseHandler):
+    crawl_config = {
+    }
+
+    def __init__(self):
+        self.rootUrl = 'http://sh.lianjia.com/ershoufang'
+        self.count = 0
+
+    @every(minutes=24 * 60)
+    def on_start(self):
+        self.crawl(self.rootUrl,
+                   callback=self.index_page)
+
+    @config(age=10 * 24 * 60 * 60)
+    def index_page(self, response):
+
+        for each in response.doc('a.js_fanglist_title').items():
+            newFullUrl = urlparse.urljoin(self.rootUrl, each.attr.href)
+            self.crawl(newFullUrl, callback=self.detail_page)
+
+        nextButton = response.doc('a[gahref=results_next_page]')
+        if nextButton is not None:
+            newFullUrl = urlparse.urljoin(self.rootUrl, nextButton.attr.href)
+            self.crawl(newFullUrl, callback=self.index_page)
+
+    @config(priority=2)
+    def detail_page(self, response):
+        return {
+            "url": response.url,
+            "title": response.doc('h1.header-title').text(),
+        }
---
+++ HouseSpider/spider.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pyspider -c config.json
