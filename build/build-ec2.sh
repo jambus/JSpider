978e9746e90bbd0fcd993a90c6c10860fde47670
Liang Shen <jambus2010@gmail.com>
2017-11-23 11:22:11 +0800

    Update ec2 script commands

--- build/build-ec2.sh
+++ build/build-ec2.sh
@@ -20,12 +20,15 @@ if [ $? -ne 0 ]; then
 	instanceSecurityGroup=`aws ec2 create-security-group --group-name ec2-spider-securitygroup --description "ec2-spider security group" --query 'GroupId' | sed 's/"//g'`
 
 	aws ec2 authorize-security-group-ingress --group-name ec2-spider-securitygroup --protocol tcp --port 22 --cidr 0.0.0.0/0
-	aws ec2 authorize-security-group-ingress --group-name ec2-spider-securitygroup --protocol tcp --port 8080 --cidr 0.0.0.0/0
+	aws ec2 authorize-security-group-ingress --group-name ec2-spider-securitygroup --protocol tcp --port 5000 --cidr 0.0.0.0/0
 	aws ec2 authorize-security-group-ingress --group-name ec2-spider-securitygroup --protocol tcp --port 80 --cidr 0.0.0.0/0
+	aws ec2 authorize-security-group-ingress --group-name ec2-spider-securitygroup --protocol tcp --port 5432 --cidr 0.0.0.0/0
+
+	echo "Security group created: ${green}$instanceSecurityGroup${reset}"
 else
 	instanceSecurityGroup=`echo $instanceSecurityGroup | sed 's/"//g'`
+	echo "Security group used: ${green}$instanceSecurityGroup${reset}"
 fi
-echo "Security group created: ${green}$instanceSecurityGroup${reset}"
 
 instanceResourceId=`aws ec2 run-instances --image-id ami-15872773 --count 1 --instance-type t2.micro --key-name jambus2018-ec2 --security-group-ids $instanceSecurityGroup \
 	--query 'Instances[0].InstanceId' \
@@ -38,9 +41,12 @@ instanceResourceId=`aws ec2 run-instances --image-id ami-15872773 --count 1 --in
 echo "Instance Resource ID created: ${green}$instanceResourceId${reset}"
 aws ec2 create-tags --resources $instanceResourceId --tags Key=Name,Value=jambus_spider
 
-echo "ec2 instance start to create. Use below command to connect with SSH:"
+instancePublicDNS=`aws ec2 describe-instances --instance-ids $instanceResourceId --query 'Reservations[0].Instances[0].PublicDnsName' | sed 's/"//g'`
 #aws ec2 describe-instances --filters "Name=tag:Name,Values=jambus_spider" --query 'Reservations[0].Instances[0].PublicDnsName' | sed 's/"//g' | xargs -I {} echo "ssh -i ~/jambus2018-ec2.pem ubuntu@{}"
-aws ec2 describe-instances --instance-ids $instanceResourceId --query 'Reservations[0].Instances[0].PublicDnsName' | sed 's/"//g' | xargs -I {} echo "${green}ssh -i ~/jambus2018-ec2.pem ubuntu@{}${reset}"
+#aws ec2 describe-instances --instance-ids $instanceResourceId --query 'Reservations[0].Instances[0].PublicDnsName' | sed 's/"//g' | xargs -I {} echo "${green}ssh -i ~/jambus2018-ec2.pem ubuntu@{}${reset}"
+echo "EC2 instance start to create. PublicDnsName is: ${green}$instancePublicDNS${reset}"
+echo "Use below command to connect with SSH:"
+echo "${green}ssh -i ~/jambus2018-ec2.pem ubuntu@$instancePublicDNS${reset}"
 
 #echo "Clean up temp files"
 #rm install-software64
